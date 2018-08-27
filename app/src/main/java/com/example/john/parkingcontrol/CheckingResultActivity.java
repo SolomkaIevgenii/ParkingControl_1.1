96610cdc763d489e1e853b121cecf22fe5afcdeb
Solomka Ievgenii <solomka1989@gmail.com>
2018-08-27 11:46:35 +0300

    HttpClient+AsyncTask для вывода инвормации в CheckingResultActivity

--- app/src/main/java/com/example/john/parkingcontrol/CheckingResultActivity.java
+++ app/src/main/java/com/example/john/parkingcontrol/CheckingResultActivity.java
@@ -1,13 +1,39 @@
 package com.example.john.parkingcontrol;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CheckingResultActivity extends AppCompatActivity {
+
+    public static String enteredCarNumber;
+
+    private TextView resultView;
+    private MyAsyncTask myAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checking_result);
+
+        resultView = findViewById(R.id.textViewResult);
+
+        myAsyncTask = new MyAsyncTask(resultView);
+        myAsyncTask.execute("http://parking.2click.money/ExtApi/carState?id="+enteredCarNumber+"&accToken=fdf909e4j3f03jikdsjfpsdg9sdfd0ifjsdik");
+    }
+
+    public static class MyAsyncTask extends AsyncTask<String, Integer, String>{
+
+        private TextView resultView;
+
+        public MyAsyncTask(TextView resultView){
+            this.resultView = resultView;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/john/parkingcontrol/EnterNumberActivity.java
+++ app/src/main/java/com/example/john/parkingcontrol/EnterNumberActivity.java
@@ -1,13 +1,31 @@
 package com.example.john.parkingcontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EnterNumberActivity extends AppCompatActivity {
+
+    EditText carNumber;
+    String finalCarNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_number);
+
+        carNumber = findViewById(R.id.editTextCarNumber);
+        finalCarNumber = carNumber.getText().toString();
+
+        findViewById(R.id.buttonCheck).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), CheckingResultActivity.class);
+                intent.putExtra(CheckingResultActivity.enteredCarNumber, finalCarNumber);
+                startActivity(intent);
+            }
+        });
     }
 }
